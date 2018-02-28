f0315e00edea2ff14507c13c1219069438261666
Mo Mirza <mo.mirza@iwoca.co.uk>
2018-02-28 21:37:54 +0000

    Added missing stage

--- code/flask-backend/app/hello.py
+++ code/flask-backend/app/hello.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
+
+from pymongo import MongoClient
 import json
 
 app = Flask(__name__)
+CORS(app)
+
+client = MongoClient("db", 27017)
 
 
 @app.route("/")
 def hello():
-    return json.dumps("Hello World!")
+    return json.dumps(client['test']['welcome'].find_one()['welcome_text'])
